b41d1789894141f0da459e893446d30a1aa3efd8
Phinjock Sherpa <phinjock@gmail.com>
2022-09-18 16:44:53 -0700

    views folder and public folder

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 
 const express = require("express");
 const path = require("path");
-const sequelize = require("./config/connection");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const Sequelize = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
