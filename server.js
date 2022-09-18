49446c3753fe70ad62a30159f7a1103ce8e62c2a
Phinjock Sherpa <phinjock@gmail.com>
2022-09-18 16:50:56 -0700

    update server.js session and handlebars access

--- server.js
+++ server.js
@@ -7,9 +7,29 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const Sequelize = require("connect-session-sequelize")(session.Store);
 
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: "secret",
+  cookie: {},
+  resave: false,
+  saveUintialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlbars", hbs.engine);
+app.set("view engine", "handlebars");
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
