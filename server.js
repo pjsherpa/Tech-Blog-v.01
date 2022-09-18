122a2e1ac7c9df13b6f73d13846bb9592cfc3e87
Phinjock Sherpa <phinjock@gmail.com>
2022-09-18 13:35:27 -0700

    created controllers folder and index.js file to create  moduler routes for api&home WIP routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
