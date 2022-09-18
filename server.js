c6b05dd0795c570ae32ff6cb82bdfaffa25169e2
Phinjock Sherpa <phinjock@gmail.com>
2022-09-18 12:12:30 -0700

    created sql connection to server with new directory db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 const express = require("express");
 const path = require("path");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+//accessing msql to connect to the db
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server started on port${PORT}`));
 });
