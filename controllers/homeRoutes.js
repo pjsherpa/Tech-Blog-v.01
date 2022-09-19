68eaee2b73f24cf3552115dde459c44962781d76
Phinjock Sherpa <phinjock@gmail.com>
2022-09-18 21:19:06 -0700

    checking errors for server, homeroutes test on going

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,24 +8,59 @@ const withAuth = require("../utils/auth");
 
 //Get all posts in homepage
 
+// router.get("/", async (req, res) => {
+//   try {
+//     // Get all data
+//     const postData = await Post.findAll({
+//       include: [User],
+//     });
+//     // Serialize data so the template can read it
+//     const posts = postData.map((post) => post.get({ plain: true }));
+
+//     // Pass serialized data and session flag into template
+
+//     res.render("all-posts-administration", {
+//       posts,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+// router.get("/", async (req, res) => {
+//   try {
+//     // Get all projects and JOIN with user data
+//     const postData = await Post.findAll({
+//       include: [
+//         {
+//           model: User,
+//           // attributes: ['name'],
+//         },
+//       ],
+//     });
+
+//     // Serialize data so the template can read it
+//     const posts = postData.map((post) => post.get({ plain: true }));
+
+//     // Pass serialized data and session flag into template
+//     res.render("all-posts-administration", {
+//       posts,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.get("/", async (req, res) => {
-  try {
-    // Get all data
-    const postData = await Post.findAll({
-      include: [User],
-    });
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-
-    res.render("all-posts-administration", {
-      posts,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  // We find all dishes in the db and set the data equal to dishData
+  const postData = await Post.findAll().catch((err) => {
+    res.json(err);
+  });
+  // We use map() to iterate over dishData and then add .get({ plain: true }) each object to serialize it.
+  const posts = postData.map((post) => post.get({ plain: true }));
+  // We render the template, 'all', passing in dishes, a new array of serialized objects.
+  res.render("all", { posts });
 });
 
 //check which id the route will be located.
