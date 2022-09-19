b6e586ca1d19cc581225c95a481fab96141cb889
Phinjock Sherpa <phinjock@gmail.com>
2022-09-18 17:02:25 -0700

    comment-routes done, WIP view&routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const { Comment, User } = require("../../models/");
+const withAuth = require("../../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    console.log(comments);
+
+    res.render("single-post", { comments, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", withAuth, async (req, res) => {
+  const body = req.body;
+
+  try {
+    const addComment = await Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    });
+    res.json(addComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
