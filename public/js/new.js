95aa53b47c6c9615483dc3e6d31bc9885bb9d675
Phinjock Sherpa <phinjock@gmail.com>
2022-09-18 19:56:18 -0700

    updated codes on new& comment.js

---
+++ public/js/new.js
@@ -0,0 +1,22 @@
+const newFormandler = async function (event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const body = document.querySelector('input[name="post-body"]').value;
+
+  await fetch("/api/post", {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  document.location.replace("/dashboard");
+};
+
+document
+  .querySelector("#new-post-form")
+  .addEventListener("submit", newFormandler);
